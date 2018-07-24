1d1791ac73330e6ade7a4bb12422a5786f20275d
iory <ab.ioryz@gmail.com>
2018-07-24 21:08:43 +0900

    [jsk_tools/test/env-hooks] Fixed test case

--- jsk_tools/test/sh/rossetmaster.sh
+++ jsk_tools/test/sh/rossetmaster.sh
@@ -3,9 +3,5 @@
 _THIS_DIR=$(builtin cd "`dirname "${BASH_SOURCE[0]}"`" > /dev/null && pwd)
 source $_THIS_DIR/../../env-hooks/99.jsk_tools.bash
 
-ECHO_OUTPUT="$1"
-ip() {
-    echo "$ECHO_OUTPUT"
-}
-rossetip > /dev/null 2>&1
-echo $ROS_IP
+rossetmaster "$1" "${2-11311}" > /dev/null 2>&1
+echo $ROS_MASTER_URI
