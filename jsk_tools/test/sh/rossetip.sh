fe055de4812d4b45960d7b52bff87cff68fd7ead
iory <ab.ioryz@gmail.com>
2018-07-30 18:43:19 +0900

    [jsk_tools/test/env_hooks] Update test sh

--- jsk_tools/test/sh/rossetip.sh
+++ jsk_tools/test/sh/rossetip.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-_THIS_DIR=$(builtin cd "`dirname "${BASH_SOURCE[0]}"`" > /dev/null && pwd)
-source $_THIS_DIR/../../env-hooks/99.jsk_tools.bash
+JSK_TOOLS_PATH=$(rospack find jsk_tools)
+if [ "$JSK_TOOLS_PATH" = "/opt/ros/${ROS_DISTRO}/share/jsk_tools" ]; then
+    source /opt/ros/${ROS_DISTRO}/setup.bash
+else
+    source `catkin locate --shell-verbs`
+    cd $(rospack find jsk_tools) && source $(catkin locate)/devel/etc/catkin/profile.d/99.jsk-tools.sh
+fi
 
 ECHO_OUTPUT="$1"
 ip() {
--- jsk_tools/test/sh/rossetmaster.sh
+++ jsk_tools/test/sh/rossetmaster.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-_THIS_DIR=$(builtin cd "`dirname "${BASH_SOURCE[0]}"`" > /dev/null && pwd)
-source $_THIS_DIR/../../env-hooks/99.jsk_tools.bash
+JSK_TOOLS_PATH=$(rospack find jsk_tools)
+if [ "$JSK_TOOLS_PATH" = "/opt/ros/${ROS_DISTRO}/share/jsk_tools" ]; then
+    source /opt/ros/${ROS_DISTRO}/setup.bash
+else
+    source `catkin locate --shell-verbs`
+    cd $(rospack find jsk_tools) && source $(catkin locate)/devel/etc/catkin/profile.d/99.jsk-tools.sh
+fi
 
 rossetmaster "$1" "${2-11311}" > /dev/null 2>&1
 echo $ROS_MASTER_URI
